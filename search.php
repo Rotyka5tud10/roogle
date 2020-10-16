59038250b708312557401362d83f8c0f3e793e9a
Rotyka5tud10 <52159777+Rotyka5tud10@users.noreply.github.com>
2020-10-16 22:57:00 +0200

    Add files via upload

---
+++ search.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Nincs találat a következőre: "<?php
+        echo $_POST["value"];
+        ?>"
+    </body>
+</html>
